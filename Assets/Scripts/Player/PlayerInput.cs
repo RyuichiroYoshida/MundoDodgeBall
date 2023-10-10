d61218e6c57cf307c673e7a31a72b70ba40d1365
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-10-10 23:28:13 +0900

    [add] PlayerInput入力受け取り追加

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -4,22 +4,23 @@ using UnityEngine.InputSystem;
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private InputAction _action;
-
+    private float _value = 0;
+    public float Value => _value;
     private void OnEnable()
     {
+        _action.performed += OnPerformed;
         _action?.Enable();
     }
 
     private void OnDisable()
     {
+        _action.performed -= OnPerformed;
         _action?.Disable();
     }
     
-    private void Update()
+    private void OnPerformed(InputAction.CallbackContext context)
     {
-        if (_action == null)
-            return;
-        var value = _action.ReadValue<float>();
-        print($"Actionの入力値 {value}");
+        _value = context.ReadValue<float>();
+        print($"Action : {_value}");
     }
 }
