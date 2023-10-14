2cd2063eced9e4b4a9ef3e306852908c25a0b11c
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-10-14 18:06:16 +0900

    [add] Player移動のInputSystem化完了

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -7,15 +7,38 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float _moveSpeed = 10;
     [SerializeField] private float _jumpForce = 5;
 
-    private Rigidbody _rigidbody;
-    private Playerinput _playerinput;
-    private void Start()
+    private Rigidbody _rb;
+    private Playerinput _playerInput;
+    private Vector2 _moveInputValue;
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        _playerInput = new Playerinput();
+        _playerInput.Player.Move.started += OnMove;
+        _playerInput.Player.Move.performed += OnMove;
+        _playerInput.Player.Jump.started += OnJump;
+        _playerInput.Player.Jump.performed += OnJump;
+        _playerInput.Enable();
     }
 
-    private void Update()
+    private void OnDestroy()
     {
-        
+        _playerInput?.Dispose();
+    }
+    
+    private void FixedUpdate()
+    {
+        _rb.AddForce(new Vector3(_moveInputValue.x, 0, _moveInputValue.y) * _moveSpeed);
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        _moveInputValue = context.ReadValue<Vector2>();
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
     }
 }
